0e645d95a8ed27d7d53a98f7da52960fb284c968
elcarvalho <lessa.eric@gmail.com>
2020-03-08 12:25:00 -0300

    chore(ambiente): criando projeto

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+const App = () => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Step One</Text>
+      <Text style={styles.sectionDescription}>
+        Edit <Text style={styles.highlight}>App.js</Text> to change this screen
+        and then come back to see your edits.
+      </Text>
+    </View>
+  );
+};
+
+export default App;
