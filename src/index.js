69b7b91d8d2bbfeb0b030440926532fdb434f525
elcarvalho <lessa.eric@gmail.com>
2020-03-10 18:07:08 -0300

    chore(ambiente): configurando statusbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import 'react-native-gesture-handler';
 import './config/ReactotronConfig';
+
 import React from 'react';
 
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import Routes from './routes';
@@ -10,6 +12,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
         <Routes />
       </>
     </NavigationContainer>
