3efd37f74c0710652a032d8c63856477ba774cac
elcarvalho <lessa.eric@gmail.com>
2020-03-12 16:22:08 -0300

    chore(desenvolvendo app): estilizando listagem

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,7 +5,19 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '../../services/api';
 
-import {Container, Form, Input, SubmitButton} from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
 export default function Main() {
   const [newUser, setNewUser] = useState('');
@@ -27,7 +39,6 @@ export default function Main() {
     setNewUser('');
 
     Keyboard.dismiss();
-    console.tron.log(users);
   };
 
   return (
@@ -47,6 +58,22 @@ export default function Main() {
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
+
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({item}) => (
+          <User>
+            <Avatar source={{uri: item.avatar}} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+
+            <ProfileButton onPress={() => {}}>
+              <ProfileButtonText>Ver perfil</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
     </Container>
   );
 }
